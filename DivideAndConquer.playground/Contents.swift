c1b2178301be88563fc16b6755c440f68625a18e
Sarvar019 <megafon.kcr.09.09@gmail.com>
2024-12-21 10:46:32 +0500

    update on quicksort and d&c

--- DivideAndConquer.playground/Contents.swift
+++ DivideAndConquer.playground/Contents.swift
@@ -4,16 +4,52 @@ var greeting = "Hello, playground"
 
 func sum(_ array: [Int]) -> Int {
     
-    var first = array[0]
-    var tempArr = array
-    tempArr.remove(at: 0)
+    if array.isEmpty {
+        return 0
+    }
     
-    if tempArr.count == 1 {
-        return tempArr[0]
+    return array[0] + sum(Array(array.dropFirst()))
+        
+}
+sum([1, 2, 3, 4, 5])
+
+
+func length(of array: [Int]) -> Int {
+    
+    if array.isEmpty { return 0 }
+    
+    var count = 0
+    
+    var tempArray = array
+    tempArray.removeFirst()
+    
+    return  1 + length(of: tempArray)
+    
+}
+
+length(of: [34, 5, 23, 76, 13, 76, 123])
+
+
+func findMax(in array: [Int]) -> Int {
+    
+    if array.count == 1 {
+        return array[0]
+    }
+    
+    
+    
+    return 0
+}
+func ekub(num1: Int, num2: Int) -> Int {
+    
+    if num1 == num2 {
+        
     } else {
-       return first + sum(tempArr)
         
     }
     
+    return 0
 }
-sum([1, 2, 3, 4, 5])
+print("kdjfv")
+print("sdnfm vlkjev")
+
--- Quicksort.playground/Contents.swift
+++ Quicksort.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+func qSort(_ array: [Int]) -> [Int] {
+    
+    if array.count < 2 {
+        return array
+    }
+    var tempArray = array
+    let randomIndex = Int.random(in: 0...(array.count-1))
+    let pivot = array[randomIndex]
+    
+    let less = tempArray.filter({  pivot > $0  })
+    let great = tempArray.filter({  pivot < $0  })
+    print("\(less) + \([pivot]) + \(great)")
+    
+    return qSort(less)
+    +
+    [pivot]
+    +
+    qSort(great)
+}
+let myArr = [1, 43, 76, 2, 4, 6, 7, 8]
+let result = qSort(myArr)
+print(result)
