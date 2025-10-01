ef56002088f3f3bd4a9365248351c430f2c04e16
Boltaboyev Sarvarbek <boltaboyev708@gmail.com>
2025-10-01 13:38:53 +0500

    mergeAlternately

--- NeetCode/Two_Pointers.playground/Contents.swift
+++ NeetCode/Two_Pointers.playground/Contents.swift
@@ -40,9 +40,9 @@ func validPalindrome(_ s: String) -> Bool {
     
     while left <= right {
         if s[left] != s[right] {
-          if isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1) {
-            return false
-          }
+            if isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1) {
+                return false
+            }
             
         }
         left += 1
@@ -50,7 +50,7 @@ func validPalindrome(_ s: String) -> Bool {
     }
     
     return true
-   }
+}
 
 func isPalindrome(_ s: [Character], _ left:  Int, _ right:  Int) -> Bool {
     
@@ -67,3 +67,32 @@ func isPalindrome(_ s: [Character], _ left:  Int, _ right:  Int) -> Bool {
     
     return true
 }
+
+// MARK: - mergeAlternately
+func mergeAlternately(_ word1: String, _ word2: String) -> String {
+    let word1: [Character] = Array(word1)
+    let word2: [Character] = Array(word2)
+    
+    var left = 0
+    var right = word1.count > word2.count ? word2.count - 1 : word1.count - 1
+
+    var ans = ""
+    
+    while left <= right {
+        
+        ans += String(word1[left])
+        ans += String(word2[left])
+        
+        left += 1
+    }
+    if word1.count != word2.count {
+        ans += word1.count > word2.count ?
+        word1[left...(word1.count - 1)] :
+        word2[left...(word2.count - 1)]
+    }
+    
+    return ans
+}
+
+mergeAlternately("abc", "pqr")
+"sbaarra"
